fc9a38384930bdf2c2fc6ed5afb46212c802b9a9
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-02-15 14:08:12 -0800

    added method functionality for start and stop scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
 void main() {
   runApp(const App());
@@ -6,6 +9,40 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+
+  void _startNFCScanSession() async {
+    print('Scanning started...');
+
+    await NfcManager.instance.startSession(
+      // These polling options will help us read our YubiKey.
+      // The YubiKey will use read at iso14443
+      pollingOptions: {
+        NfcPollingOption.iso14443,
+      },
+      onDiscovered: (NfcTag tag) async {
+        // When a tag is detected, we will add functionality here
+        // to decode the tag and stop the NFC scanning.
+        print('Tag Detected!');
+
+        Ndef? ndef = Ndef.from(tag);
+
+        if (ndef == null) {
+          print('Tag not compatible with NDEF');
+          return;
+        }
+
+        print('Tag Data: ${tag.data}');
+
+        // Stop NFC Scan Session now that we have read our tag
+        // and accessed the data.
+        _stopNFCScanSession();
+      },
+    );
+  }
+
+  void _stopNFCScanSession() async {
+    await NfcManager.instance.stopSession();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +64,7 @@ class App extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _startNFCScanSession,
                 child: const Text(
                   'Start Scanning',
                 ),
